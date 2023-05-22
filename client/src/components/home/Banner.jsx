a5648978ba179163f2c2b370230cf1ee32dfc1bf
mayank-Pareek <mpareek365@gmail.com>
2023-05-22 19:17:16 -0400

    Add banner carousel

---
+++ client/src/components/home/Banner.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { bannerData } from "../../constants/data";
+import styled from "@emotion/styled";
+const responsive = {
+  desktop: {
+    breakpoint: { max: 4000, min: 1024 },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+const Image = styled("img")({
+  width: "100%",
+  height: "280px",
+});
+
+const Banner = () => {
+  return (
+    <Carousel
+      responsive={responsive}
+      dotListClass="custom-dot-list-style"
+      itemClass="carousel-itm-padding-40px"
+      containerClass="carousel-container"
+      infinite={true}
+      swipeable={false}
+      draggable={false}
+      showDots={false}
+      autoPlay={true}
+      autoPlaySpeed={4000}
+      keyBoardControl={true}
+      slidesToSlide={1}
+    >
+      {bannerData.map((item) => (
+        <Image src={item.url} alt="product banner" />
+      ))}
+    </Carousel>
+  );
+};
+
+export default Banner;
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-import NavBar from './NavBar' 
+import React from "react";
+import NavBar from "./NavBar";
+import Banner from "./Banner";
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+
+const Container = styled(Box)`
+  padding: 10px 10px;
+  background-color: #f2f2f2;
+`;
 const Home = () => {
   return (
-    <NavBar/>
-  )
-}
+    <>
+      <NavBar />
+      <Container>
+        <Banner />
+      </Container>
+    </>
+  );
+};
 
-export default Home
+export default Home;
