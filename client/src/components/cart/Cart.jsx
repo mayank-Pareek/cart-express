63e52c7118555c85821148a3d2bdf5d5c52d4a6b
mayank-Pareek <mpareek365@gmail.com>
2023-06-05 14:10:34 -0400

    Create layout for cart

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -1,5 +1,20 @@
+import { Box, Grid, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const { cartItems } = useSelector((state) => state.cart);
+
+  return( <>{cartItems.length ? <Grid container>
+    <Grid item lg={9} md={9} sm={12} xs={12}>
+        <Box><Typography>My Cart ({cartItems.length})</Typography></Box>
+        {
+            // cartItems.map(item=>(
+
+            // ))
+        }
+    </Grid>
+    <Grid item lg={3} md={3} sm={12} xs={12}></Grid>
+  </Grid> : <div>No items</div>}</>);
 };
 
 export default Cart;
