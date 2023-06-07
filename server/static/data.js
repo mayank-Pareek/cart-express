7558399ce6b6a3e85f3d969bb2fcddfb01c60493
mayank-Pareek <mpareek365@gmail.com>
2023-06-07 01:14:27 -0400

    Update products data

--- server/static/data.js
+++ server/static/data.js
@@ -21,23 +21,23 @@ export const products = [
   },
   {
     id: "product2",
-    url: "https://rukminim1.flixcart.com/image/416/416/kl6wx3k0/sandwich-maker/8/r/d/sandwich-01-flipkart-smartbuy-original-imagydds4zthxt8z.jpeg?q=70",
-    detailUrl:
-      "https://rukminim1.flixcart.com/image/416/416/kl6wx3k0/sandwich-maker/8/r/d/sandwich-01-flipkart-smartbuy-original-imagydds4zthxt8z.jpeg?q=70",
+    url: "https://m.media-amazon.com/images/I/61uw-JTPfhL._AC_UL600_FMwebp_QL65_.jpg",
+    detailUrl: "https://m.media-amazon.com/images/I/61uw-JTPfhL._AC_SY741_.jpg",
     title: {
-      shortTitle: "Sandwich Makers",
-      longTitle: "Canadian Classics Sandwich Maker  (Black)",
+      shortTitle: "Samsung Galaxy S20 FE",
+      longTitle:
+        "Samsung Galaxy S20 FE (5G) 128GB (Canadian Model G781W) 6.5' Display Unlocked Smartphone - Cloud Navy",
     },
     price: {
-      mrp: 49,
-      cost: 34,
-      discount: "30%",
+      mrp: 328,
+      cost: 282,
+      discount: "13%",
     },
     quantity: 1,
     description:
-      "This non-stick sandwich toaster .easy to use and very handy. Directly hold over flame to make tasty toasts and toasted sandwiches. Specially designed by keeping your needs in mind, the sandwich maker makes whatever youre doing simpler, smarter and better",
-    discount: "30% Off",
-    tagline: "Best Value",
+      "The Samsung S20 FE brings flagship features at an affordable price. Packed with a powerful processor, stunning Super AMOLED display, and a versatile triple camera system, it delivers exceptional performance and captures brilliant photos and videos. With a sleek design and a range of vibrant colors to choose from, the Samsung S20 FE is the perfect blend of style and functionality. Experience the latest technology and premium features without breaking the bank with the Samsung S20 FE.",
+    discount: "Free Shipping",
+    tagline: "New Launch",
   },
   {
     id: "product3",
@@ -163,43 +163,41 @@ export const products = [
   },
   {
     id: "product9",
-    url: "https://m.media-amazon.com/images/I/81LK5OudO9L._AC_UY327_FMwebp_QL65_.jpg",
-    detailUrl:
-      "https://rukminim1.flixcart.com/image/416/416/kk01pjk0/fan/d/d/l/tiktik-quiet-portable-table-fan-zigma-original-imafzg7ftzuckpad.jpeg?q=70",
+    url: "https://m.media-amazon.com/images/I/71y7t9RATsL._AC_UL600_FMwebp_QL65_.jpg",
+    detailUrl: "https://m.media-amazon.com/images/I/71y7t9RATsL._AC_UX679_.jpg",
     title: {
-      shortTitle: "Dell Inspiron 15 3000",
+      shortTitle: "Hawaiian Shirt",
       longTitle:
-        "Dell Inspiron 15 3000 Series 3511 Laptop, 15.6' FHD Display, 11th Gen Intel Core i5-1135G7 Quad-Core Processor 16GB RAM 500GB SSD Webcam",
+        "Men Button Down Shirt Casual Hawaiian Shirt Short Sleeve Aloha Shirt for Beach Party",
     },
     price: {
-      mrp: 769,
-      cost: 699,
-      discount: "10%",
+      mrp: 39,
+      cost: 46,
+      discount: "15%",
     },
     quantity: 1,
     description:
-      " 11th Generation Intel Core i5-1135G7 Processor (8MB Cache, 2.4 GHz), Windows 11 Home 64-bit English, Intel UHD Graphics with shared graphics memory. 15.6-inch FHD (1920 x 1080) Anti-glare LED Backlight Non-Touch Narrow Border WVA Display, 3-Cell Battery, 41WHr (Integrated), 802.11ac 1x1 WiFi and Bluetooth",
-    discount: "10% Off",
+      "Men's hawaiian shirts are featured with lapel, short sleeves, curved hem, top left pocket and straight fit with button closure on the front. Very cool to wear and the men's novelty shirt goes perfectly with shorts, beach pants, jeans and more.",
+    discount: "Upto 15% Off",
     tagline: "Top Selling",
   },
   {
     id: "product10",
-    url: "https://m.media-amazon.com/images/I/51MVojpiZcL._AC_UF452,452_FMjpg_.jpg",
-    detailUrl:
-      "https://m.media-amazon.com/images/I/61+5r8u3-+L._AC_SL1500_.jpg",
+    url: "https://m.media-amazon.com/images/I/618ptI1RbTL._AC_UL600_FMwebp_QL65_.jpg",
+    detailUrl: "https://m.media-amazon.com/images/I/61OR71baCRL._AC_UX679_.jpg",
     title: {
-      shortTitle: "TCL 43' Class 4-Series",
-      longTitle: "TCL 43` Class 4-Series 4K UHD HDR Smart Roku TV - 43S455-CA ",
+      shortTitle: "Men's T-Shirt",
+      longTitle: "Jerzees Men's Long-Sleeve T-Shirt",
     },
     price: {
-      mrp: 329,
-      cost: 299,
-      discount: "9%",
+      mrp: 13,
+      cost: 15,
+      discount: "12%",
     },
     quantity: 1,
     description:
-      "Stunning 4K Ultra HD: Enjoy breathtaking 4K movies and TV shows at 4 times the resolution of Full HD. Plus your favorite HD shows, movies, and sporting events are enhanced to near Ultra HD resolution with advanced 4K Upscaling. HDR delivers bright and accurate colors for a lifelike viewing experience.",
-    discount: "10% Off",
+      "Stay cool and stylish with our trendy t-shirt. Made with high-quality fabric, this comfortable t-shirt is designed to elevate your casual wardrobe. Its modern fit and vibrant color options make it a versatile choice for any occasion. Express your unique style effortlessly with this must-have t-shirt that combines comfort and fashion in one.",
+    discount: "Over 10% Off",
     tagline: "Free Shipping",
   },
   {
@@ -221,5 +219,46 @@ export const products = [
       "Completely rebuilt from the ground up using Naughty Dog’s latest PS5 engine technology to improve every visual detail, The Last of Us experience has been faithfully enhanced with more realistic lighting and atmosphere, more intricate environments and creative reimaginings of familiar spaces.",
     discount: "25% Off",
     tagline: "New Offer",
+  },
+  {
+    id: "product12",
+    url: "https://rukminim1.flixcart.com/image/416/416/kl6wx3k0/sandwich-maker/8/r/d/sandwich-01-flipkart-smartbuy-original-imagydds4zthxt8z.jpeg?q=70",
+    detailUrl:
+      "https://rukminim1.flixcart.com/image/416/416/kl6wx3k0/sandwich-maker/8/r/d/sandwich-01-flipkart-smartbuy-original-imagydds4zthxt8z.jpeg?q=70",
+    title: {
+      shortTitle: "Sandwich Makers",
+      longTitle: "Canadian Classics Sandwich Maker  (Black)",
+    },
+    price: {
+      mrp: 49,
+      cost: 34,
+      discount: "30%",
+    },
+    quantity: 1,
+    description:
+      "This non-stick sandwich toaster .easy to use and very handy. Directly hold over flame to make tasty toasts and toasted sandwiches. Specially designed by keeping your needs in mind, the sandwich maker makes whatever youre doing simpler, smarter and better",
+    discount: "30% Off",
+    tagline: "Best Value",
+  },
+  {
+    id: "product13",
+    url: "https://m.media-amazon.com/images/I/71aMrvCVXcL._AC_UL600_FMwebp_QL65_.jpg",
+    detailUrl:
+      "https://m.media-amazon.com/images/I/71aMrvCVXcL._AC_SL1500_.jpg",
+    title: {
+      shortTitle: "Android 11 3G Phone",
+      longTitle:
+        "Android 11 3G Phone, Note 6 Unlocked Smartphone, 6.1” HD+ Full Screen, Quad-core 1GB+32GB Mobile Phone, 3300mAh Battery",
+    },
+    price: {
+      mrp: 150,
+      cost: 109,
+      discount: "27%",
+    },
+    quantity: 1,
+    description:
+      "This cutting-edge device combines seamless performance, innovative features, and a sleek design to redefine your smartphone experience. With a stunning display that brings your content to life, powerful processing capabilities to handle any task effortlessly, and a high-resolution camera that captures every moment in stunning detail, this phone is a true companion for your everyday adventures. Stay connected, productive, and entertained with this exceptional device that offers reliability and versatility without compromising on style.",
+    discount: "Upto 30% off",
+    tagline: "Best Seller",
   },
 ];
